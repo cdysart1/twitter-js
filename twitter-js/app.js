d8e101927a4b59bd5ae05b73838918a215edfe3e
Carol Dysart <dysart.carol@gmail.com>
2017-06-12 22:11:23 -0400

    updated

--- twitter-js/app.js
+++ twitter-js/app.js
@@ -1,10 +1,40 @@
 const express = require( 'express' );
+const nunjucks = require( 'nunjucks' );
 const app = express(); // creates an instance of an express application
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+app.use(function (req, res, next) {
+    // do your logging here
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+  console.log();
+  next();
+})
+
+app.get('/', function (req, res) {
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+res.render( 'index', {title: 'Hall of Fame', people: people} );
+})
 
 app.listen(3000, function(){
   console.log('server listening');
 });
 
-app.get('/', function (req, res) {
-  res.send('hello!');
-})
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+
+nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
+
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});
+
